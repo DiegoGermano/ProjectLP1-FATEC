c16b678659f658459a36f4deecc12b818b226871
Diego Germano <diego.germano@outlook.com>
2015-03-18 23:27:20 -0300

    Criado Builders para as intruções sql's

--- Back-End/SpacedRepetitionSoftware/main.c
+++ Back-End/SpacedRepetitionSoftware/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <mysql.h>
 #include <string.h>
+#include <time.h>
 
+//##############     Auxiliary functions ###################################################
 
 void remove_all_chars(char* str, char c) {
 
@@ -22,32 +24,288 @@ void finish_with_error(MYSQL *con)
   exit(1);
 }
 
+int count_characters(const char *str, char character)
+{
+    const char *p = str;
+    int count = 0;
+
+    do {
+        if (*p == character)
+            count++;
+    } while (*(p++));
+
+    return count;
+}
+
+int random_number(int min_num, int max_num)
+{
+    int result=0,low_num=0,hi_num=0;
+    if(min_num<max_num)
+    {
+        low_num=min_num;
+        hi_num=max_num+1; // this is done to include max_num in output.
+    }else{
+        low_num=max_num+1;// this is done to include max_num in output.
+        hi_num=min_num;
+    }
+    srand(time(NULL));
+    result = (rand()%(hi_num-low_num))+low_num;
+    return result;
+}
+//##########################################################################################
+
+
+typedef struct part
+{
+   char * value[500];
+
+}part;
+
+char * buildCmdInsert(char *value, char *table)
+{
+    int i, j =0;
+
+    char newValue[strlen(value)];
+
+	for(i=11; i<= strlen(value) ; i++)
+	{
+        newValue[j] = value[i];
+        j++;
+	}
+
+	remove_all_chars(newValue, '}');
+	remove_all_chars(newValue, '\\');
+
+	int number_of_values = (count_characters(value, ':') * 2);
+
+    part fieldsValues[number_of_values];
+
+    char * pch;
+    int c = 0;
+
+    pch = strtok (newValue,":,");
+    while (pch != NULL)
+    {
+
+        strcpy(fieldsValues[c].value, pch);
+        pch = strtok (NULL, ":,");
+
+        c++;
+    }
+
+     char fields[strlen(value)];
+     char values[strlen(value)];
+     char sqlInsert[strlen(value) + 100];
+
+     strcpy(fields, "(");
+     strcpy(values, "(");
+
+     strcpy(sqlInsert, "INSERT INTO  ");
+     strcat(sqlInsert, table);
+     strcat(sqlInsert, " ");
+
+     int isFieldInvalid = 1;
+
+     for(i = 0; i < number_of_values; i++)
+     {
+         if (strstr(fieldsValues[i].value, "$$hashKey") != NULL)
+            isFieldInvalid = 0;
+
+         if(i % 2 == 0 && isFieldInvalid)
+         {
+            strcat(fields, fieldsValues[i].value);
+            strcat(fields, ",");
+         }
+         else if(i % 2 == 1)
+         {
+             if(isFieldInvalid)
+             {
+                 strcat(values, fieldsValues[i].value);
+                 strcat(values, ",");
+             }
+             else
+             {
+                 isFieldInvalid= 1;
+             }
+         }
+     }
+
+	remove_all_chars(fields, '"'); // REMOVE ASPAS DOS CAMPOS
+
+    fields[strlen(fields)-1] = '\0';   //|
+    values[strlen(values)-1] = '\0'; //|  REMOVE ULTIMA VIRGULA DOS CAMPOS E DOS VALORES
+
+    strcat(fields, ")");  //
+    strcat(values, ")"); // FECHA VALORES E CAMPOS
+
+    strcat(sqlInsert, fields);      //|
+    strcat(sqlInsert, " VALUES ");  //| MONTA SQL INSERT
+    strcat(sqlInsert, values);     //|
+
+    return sqlInsert;
+}
+
+char * buildCmdUpdate(char *value, char *table)
+{
+    int i, j =0;
+
+    char newValue[strlen(value)];
+
+	for(i=10; i<= strlen(value) ; i++)
+	{
+        newValue[j] = value[i];
+        j++;
+	}
+
+	remove_all_chars(newValue, '}');
+
+	int number_of_values = (count_characters(value, ':') * 2);
+
+    part fieldsValues[number_of_values];
+
+    char * pch;
+    int c = 0;
+
+    pch = strtok (newValue,":,");
+    while (pch != NULL)
+    {
+
+        strcpy(fieldsValues[c].value, pch);
+        pch = strtok (NULL, ":,");
+
+        c++;
+    }
+
+     char fields[strlen(value)];
+     char values[strlen(value)];
+     char sqlUpdate[strlen(value) + 100];
+     char predicate[50];
+
+     strcpy(fields, "");
+     strcpy(values, "");
+
+     strcpy(sqlUpdate, "UPDATE ");
+     strcat(sqlUpdate, table);
+     strcat(sqlUpdate, " SET ");
+     strcpy(predicate, " WHERE ID = ");
+
+     for(i = 0; i < number_of_values; i++)
+     {
+
+         if(i % 2 == 0)
+         {
+            if(i != 0)
+            {
+                remove_all_chars(fieldsValues[i].value, '"');
+                strcat(fields, fieldsValues[i].value);
+                strcat(fields, " = ");
+            }
+         }
+         else
+         {
+            if(i == 1)
+            {
+                remove_all_chars(fieldsValues[i].value, '"');
+                strcat(predicate, fieldsValues[i].value);
+            }
+            else
+            {
+                strcat(fields, fieldsValues[i].value);
+                strcat(fields, ",");
+            }
+         }
+
+     }
+
+    fields[strlen(fields)-1] = '\0';
+    strcat(sqlUpdate, fields);
+    strcat(sqlUpdate, predicate);
+
+    return sqlUpdate;
+}
+
+char * buildCmdDelete(char *value, char *table)
+{
+    int i, j =0;
+
+    char newValue[strlen(value)];
+
+	for(i=10; i<= strlen(value)  ; i++)
+	{
+        newValue[j] = value[i];
+        j++;
+	}
+
+    char sqlDelete[strlen(value) + 50];
+
+     strcpy(sqlDelete, "DELETE FROM  ");
+     strcat(sqlDelete, table);
+     strcat(sqlDelete, " WHERE ID = ");
+     strcat(sqlDelete,newValue);
+
+    return sqlDelete;
+}
+
+char * buildCmdSelect(char *field, char *value, char *table)
+{
+
+    char sqlSelect[500];
+
+     strcpy(sqlSelect, "SELECT * FROM  ");
+     strcat(sqlSelect, table);
+     strcat(sqlSelect, " WHERE ");
+     strcat(sqlSelect, field);
+     strcat(sqlSelect, " = ");
+     strcat(sqlSelect, value);
+
+    return sqlSelect;
+}
 
 void CreateDeck(char *str)
 {
-    //cre/deck/\{"descricao":"maroto","dificuldade":"hard","idioma":"ingles"\}
+    char *result = buildCmdInsert(str , "Deck");
 
-    printf("%s", str);
+    printf("\n\n%s\n", result);
 
     return 0;
-
       MYSQL *con = mysql_init(NULL);
-
       if (con == NULL)
       {
           fprintf(stderr, "mysql_init() failed\n");
           exit(1);
       }
-
       if (mysql_real_connect(con, "127.0.0.1", "root", "lexos2009","spacedrepetition", 0, NULL, 0) == NULL) finish_with_error(con);
-
-
-
       if (mysql_query(con, "insert into deck (dificuldade, modifiedOn, CreatedOn, Ativado, descricao, Deck_Id) values (1,NOW(),NOW(),1,'uhUuuuuuuuuUUuuUuu',5)")) finish_with_error(con);
-
-
-       printf("Congratilations one deck was created :)");
+      printf("Congratilations one deck was created :)");
 }
+
+void CreateWord(char *str)
+{
+    char *result = buildCmdInsert(str , "Word");
+
+    printf("\n\n%s\n", result);
+
+    return 0;
+}
+
+void CreateUser(char *str)
+{
+    char *result = buildCmdInsert(str , "User");
+
+    printf("666");
+
+    return 0;
+}
+
+void CreateContact(char *str)
+{
+    char *result = buildCmdInsert(str , "Contact");
+
+    printf("\n\n%s\n", result);
+
+    return 0;
+}
+
 
 void Create(char *str)
 {
@@ -58,11 +316,15 @@ void Create(char *str)
     }
     else if(str[4] == 'w' && str[5] == 'o' && str[6] == 'r' && str[7] == 'd' && str[8] == '/')
     {
-       printf("Create word");
+       CreateWord(str);
     }
     else if(str[4] == 'u' && str[5] == 's' && str[6] == 'e' && str[7] == 'r' && str[8] == '/')
     {
-       printf("Create usuario");
+       CreateUser(str);
+    }
+    else if(str[4] == 'c' && str[5] == 'o' && str[6] == 'n' && str[7] == 't' && str[8] == '/')
+    {
+       CreateContact(str);
     }
     else
     {
@@ -137,33 +399,43 @@ void Read(char *str)
     }
 }
 
+void Login(char *str)
+{
+    //ROTAS DECK/WROD/USER
+    if(str[4] == 'l' && str[5] == 'o' && str[6] == 'g' && str[7] == 'i' && str[8] == '/')
+    {
+       GenarateToken();
+    }
+    else
+    {
+        printf("Error invalid argument! 00009");
+        return 0;
+    }
+}
+
+void GenarateToken()
+{
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+
+    //printf("now: %d-%d-%d %d:%d:%d\n%d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, tm.tm_isdst);
+
+    int i;
+    char randstuff[29] = {'a', 'b', 'c', 'd', 'e', 'f','g', 'w', 'F', 'f', 'E', 'W','A', 'B', 'q', 'y', 'E', 'F','A', 'B', 'C', 'D', 'E', 'F','c', 'B', 'C', 'D', 'E', 'F','A', 'B', 'C', 'D', 'E', 'F'};
+
+    for(i = 0; i <= 30; i++)
+    {
+        printf("%d",random_number(i*2,99*i+tm.tm_sec));
+        char unknown;
+        unknown = (rand() % 8) + randstuff[i];
+        printf("%c",unknown);
+    }
+
+}
 
 int main(int argc, char *argv[])
 {
-
-    //find solution to build where
-
-	int i, j =0, c = 0;
     printf("Content-type: text/plain\n\n");
-
-    char* value = "cre/deck/\{\"descricao\":\"maroto\",\"dificuldade\":\"hard\",\"idioma\":\"ingles\"\}";
-    char saida[strlen(value)-10];
-
-    char campos[150];
-
-	for(i=10; i< strlen(value); i++)
-	{
-        saida[j] = value[i];
-        j++;
-
-
-	}
-
-	 remove_all_chars(saida, '"');
-
-    printf("Saida: %s \n", saida);
-
-    return 0;
 
     if(argc <= 1 || strlen(argv[1]) < 9)
     {
@@ -189,12 +461,19 @@ int main(int argc, char *argv[])
     {
         Read(argv[1]);
     }
+    else if(argv[1][0] == 'l' && argv[1][1] == 'o' && argv[1][2] == 'g' && argv[1][3] == '/')
+    {
+        Login(argv[1]);
+    }
     else
     {
         printf("Error invalid argument! 00002");
     }
 
     return 0;
+
+    //http://zetcode.com/db/mysqlc/
+    //http://hypeshell.com/index.php/articles/14-sql/59-mysql-c-api-programming-tutorial
 
     /*
 	int i;
--- Front-End/SpacedRepetitionSoftware/SpacedRepetitionSoftware/controllers/mainController.js
+++ Front-End/SpacedRepetitionSoftware/SpacedRepetitionSoftware/controllers/mainController.js
@@ -1,10 +1,84 @@
 app.controller('mainController', function ($scope, $http, $location) {
 
-    $scope.servidor = 'http://127.0.0.1:82/cgi-bin/SpacedRepetitionSoftware.exe?cre/deck/';
+    $scope.servidor = 'http://127.0.0.1:82/cgi-bin/SpacedRepetitionSoftware.exe?';
+
+    $scope.ROUTE_CAD_DECK = "cre/deck/";
+    $scope.ROUTE_CAD_WORD = "cre/word/";
+    $scope.ROUTE_CAD_USER = "cre/user/";
+    $scope.ROUTE_CAD_CONT = "cre/cont/";
+
+    $scope.ROUTE_CAD_LOGI = "log/logi/";
+
+    $scope.contacts = [];
+
+    //login
+
+    $scope.logIn = function (user) {
+
+        $http.get($scope.servidor + $scope.ROUTE_CAD_LOGI + user.email + "/" + user.password).
+
+        success(function (data, status, headers, config) {
+
+            console.log(data);
+
+        }).
+
+        error(function (data, status, headers, config) {
+
+            console(data);
+            alert('erro: ' + data);
+
+        });
+    };
+
+
+    //Manipulations contact
+    $scope.addContact = function (contact) {
+
+        $scope.contacts.push(angular.copy(contact));
+
+    };
+
+    $scope.removeContact = function (contact) {
+
+        $scope.contacts.splice($scope.contacts.indexOf(contact), 1);
+    };
+
+
+    //Creators
+    $scope.cadastrarUser = function (user) {
+
+        $http.get($scope.servidor + $scope.ROUTE_CAD_USER + JSON.stringify(user)).
+
+        success(function (data, status, headers, config) {
+
+            for (var i = 0; i < $scope.contacts.length ; i++) {
+
+                $scope.contacts[i].usuario_id = data;
+
+                $http.get($scope.servidor + $scope.ROUTE_CAD_CONT + JSON.stringify($scope.contacts[i])).
+
+                    success(function (data, status, headers, config) {
+
+                        console.log(data);
+
+                    });
+
+            }
+            
+        }).
+
+        error(function (data, status, headers, config) {
+
+            console(data);
+            alert('erro: ' + data);
+
+        });
+    };
 
     $scope.cadastrarDeck = function (deck) {
 
-        $http.get($scope.servidor + JSON.stringify(deck) ).
+        $http.get($scope.servidor+$scope.ROUTE_CAD_DECK  + JSON.stringify(deck) ).
 
         success(function (data, status, headers, config) {
 
@@ -16,6 +90,24 @@ app.controller('mainController', function ($scope, $http, $location) {
 
             console(data);
             alert('erro: '+data);
+
+        });
+    };
+
+    $scope.cadastrarWord = function (word) {
+
+        $http.get($scope.servidor + $scope.ROUTE_CAD_WORD + JSON.stringify(word)).
+
+        success(function (data, status, headers, config) {
+
+            console.log(data);
+
+        }).
+
+        error(function (data, status, headers, config) {
+
+            console(data);
+            alert('erro: ' + data);
 
         });
     };
